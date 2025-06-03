5b535be54a41e92be7741f70054692f441c6cbb7
Viktoriia Kaspryk <kasprikviktoria4@gmail.com>
2025-06-03 14:48:55 +0200

    add nativewind library for styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,21 @@
 import { Earth } from "@/components/Earth";
 import { LinearGradient } from "expo-linear-gradient";
 import { Suspense } from "react";
-import { StyleSheet, Text } from "react-native";
+import { Text } from "react-native";
 
 export default function GlobalViewScreen() {
   return (
     <LinearGradient
-      colors={["#162B44", "#070C08"]} // różowy → niebieski
+      colors={["#162B44", "#070C08"]}
       start={{ x: 0.5, y: 0 }}
       end={{ x: 0.5, y: 1 }}
       style={{ flex: 1 }}
     >
-      <Text style={styles.text}>Earth</Text>
-      <Text style={styles.text}>21 Wed</Text>
+      <Text className="text-red-600 text-5xl">Earth</Text>
+      <Text className="text-red-600 text-5xl">21 Wed</Text>
       <Suspense fallback={null}>
         <Earth />
       </Suspense>
     </LinearGradient>
   );
 }
-
-const styles = StyleSheet.create({
-  text: {
-    color: "blue",
-    fontWeight: "bold",
-    fontSize: 30,
-  },
-});
